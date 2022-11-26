98ef16b1de5694d95f6fa7daf9d36c9ef9c39a7a
fa <zuchairul@gmail.com>
2022-11-27 00:35:20 +0800

    first init

---
+++ README.md
@@ -0,0 +1,19 @@
+## Kelompok Svelte
+
+Repository ini dibuat untuk memenuhi salah satu tugas Pemrograman Website Kelas A yang dibimbing oleh Pak Ais Prayogi, ST.,M.Eng.
+
+### Anggota:
+
+- Fauzan Adithya Zuchariul Mursalin | D121201004
+- Nur Islamiah Rifai | D121201006
+- A. Ichsan Mudatsir | D121201009
+- Arfan Bahrun | D121201010
+- Nabilah Az Zahra | D121201024
+- Muhammad Thoriq | D121201100
+- Reiky Efabras Wahyu Wijaya | D121201102
+
+### link:
+
+code: https://github.com/zzuchairul/todolist-svelte
+PPT: https://docs.google.com/presentation/d/1EfMXx1XyrNaPF8Nf9atrJ39bztrZjo3n/edit#slide=id.p1
+youtube:
---
+++ src/App.svelte
@@ -0,0 +1,73 @@
+<script>
+  let todos = ["Wake Up", "Eat"];
+  let newTodo = "";
+  let history = ["Menambahkan Eat", "Menambahkan Wak Up"];
+
+  function addTodo(item) {
+    todos = [...todos, item];
+    newTodo = "";
+    addHistory(true, item);
+  }
+
+  function removeTodo(item) {
+    todos = todos.filter((elem) => elem !== item);
+    addHistory(false, item);
+  }
+
+  function addHistory(status, item) {
+    let pesan = status ? `Menambahkan ${item}` : `${item} selesai`;
+    history = [pesan, ...history];
+  }
+</script>
+
+<div class="row">
+  <div class="todo-list">
+    <label>
+      <input type="text" bind:value={newTodo} placeholder="what will you do" />
+      <button on:click={() => addTodo(newTodo)}>Add</button>
+    </label>
+
+    <ul>
+      {#each todos as todo}
+        <li>
+          {todo} <button on:click={() => removeTodo(todo)}>Done</button>
+        </li>
+      {/each}
+    </ul>
+  </div>
+
+  <div class="history">
+    <h3>History</h3>
+    <ol>
+      {#each history as hist}
+        <li>
+          {hist}
+        </li>
+      {/each}
+    </ol>
+  </div>
+</div>
+
+<style>
+  .row {
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  .todo-list {
+    border: 1px solid;
+    width: 40%;
+    height: 80vh;
+  }
+
+  .history {
+    border: 1px solid;
+    width: 40%;
+    height: 80vh;
+  }
+
+  .history h3 {
+    display: flex;
+    justify-content: center;
+  }
+</style>
